414733902283a8435a12f22df0ce885c9e340d93
jceferino-2022238 <jceferino-2022238@kinal.edu.gt>
2024-05-14 13:47:50 -0600

    Final changes

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,7 +5,7 @@ export const NavbarC = () => {
     <div className='navbar-container'>
       <Navbar className="bg-body-tertiary" expand="lg">
       <Container fluid>
-        <Navbar.Brand>Blog del Programador, 2022238</Navbar.Brand>
+        <Navbar.Brand className='brand'>Blog del Programador, 2022238</Navbar.Brand>
         <Navbar.Toggle />
         <Navbar.Collapse className="justify-content-end">
           <Navbar.Text>
--- src/shared/hooks/getPosts.jsx
+++ src/shared/hooks/getPosts.jsx
@@ -1,24 +1,35 @@
 import { useEffect, useState } from "react";
 import { getPosts } from "../../services";
-export const useFetchPosts = () =>{
-    const [posts, setPosts] = useState([])
-    const [loading, setLoading] = useState(true)
-    useEffect(()=>{
-        const fetchPosts = async () =>{
+
+export const useFetchPosts = () => {
+    const [posts, setPosts] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchPosts = async () => {
             try {
-                const response = await getPosts()
-                if(!response.error){
-                    setPosts(response.data.posts)
-                }else{
-                    console.error('Error fetching posts:', response.message);
+                const response = await getPosts();
+                if (!response || !response.data) {
+                    throw new Error("Invalid response format");
                 }
-            } catch (e) {
-                console.error('Error fetching posts:', error);
-            } finally{
+                if (!response.error) {
+                    if (response.data.posts) {
+                        setPosts(response.data.posts);
+                    } else {
+                        console.error("Error: No posts found in response");
+                    }
+                } else {
+                    console.error("Error fetching posts:", response.message);
+                }
+            } catch (error) {
+                console.error("Error fetching posts:", error);
+            } finally {
                 setLoading(false);
             }
-        }
-        fetchPosts()
-    }, [])
-    return {posts, loading}
-}
+        };
+
+        fetchPosts();
+    }, []);
+
+    return { posts, loading };
+};
